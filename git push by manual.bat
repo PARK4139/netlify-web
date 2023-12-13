491a81ea9cfc51be99905cb815c2a1d0f555269d
JUNG HOON PARK <pjh4139@gmail.com>
2023-12-13 21:18:16 +0900

    Refer to README.md (commited at 2023 12 13 21 18 16)

--- git push by manual.bat
+++ git push by manual.bat
@@ -1,16 +1,6 @@
-:: TITLE SETTING
+:: CONSOLE SETTING
 title %~n0
-
-
-:: MINIMIZED WINDOW SETTING
-if not "%minimized%"=="" goto :minimized
-set minimized=true
-start /min cmd /C "%~dpnx0"
-goto :EOF
-:minimized
-
-
-:: VARIABLE DEFINATION SETTING
+color df
 chcp 65001 >nul
 @echo off
 @rem @echo on
@@ -19,17 +9,24 @@ for /f "delims=" %%i in ('Powershell.exe get-date -Format 'yyyy MM dd HH mm ss''
 cls
 
 
+:: MINIMIZED WINDOW SETTING
+:: if not "%minimized%"=="" goto :minimized
+:: set minimized=true
+:: start /min cmd /C "%~dpnx0"
+:: goto :EOF
+:: :minimized
+
+
 :: COMMIT MENT SETTING
 ::set commit_ment=%yyyyMMddHHmmss%
-::set commit_ment=테스트 푸쉬
-set commit_ment=작업 이력은 README.md 의 DONE 참조요함(%yyyyMMddHHmmss%)
-
+::set commit_ment=Test Push
+set commit_ment=Refer to README.md (commited at %yyyyMMddHHmmss%)
 
 
 :: GIT PUSH
 git add *  
-git commit -m "%commit_ment%" 
-git push -u origin main  
+git commit -m "%commit_ment%"
+git push -u origin main
 git status | find "working tree clean" 
 
 
@@ -45,5 +42,5 @@ explorer https://github.com/Park4139/%PROJECT_DIRECTORY_DIRNAME%
 
 
 
-:: debug
-timeout 600
+:: DEBUG SET UP
+:: timeout 600
